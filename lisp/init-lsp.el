d3c9eb8e1b7cbcaa8647a1de6b3a703cae926d2e
humphrycc <humphrycc@gmail.com>
2023-03-31 10:26:44 +0800

    C lsp

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -3,6 +3,7 @@
   ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
   (setq lsp-keymap-prefix "C-c l")
   :hook (
+         (c-mode . lsp)
          (c++-mode . lsp)
          (cmake-mode . lsp)
          ;; if you want which-key integration
